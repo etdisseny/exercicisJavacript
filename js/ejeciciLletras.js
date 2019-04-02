9ebb020d12c05b28da7f95f696ed74bb1131fc01
etdisseny <estertermens@hotmail.com>
2019-04-02 17:24:11 +0200

    exercici lletres acabat

--- js/ejeciciLletras.js
+++ js/ejeciciLletras.js
@@ -16,18 +16,55 @@ var nameList = listName.forEach(elemento=>{
 
 });
 /*fase3*/
+var letterMap = new Map(); //creo el map
 
+for(var i=0; i<listName.length; i++){
+    if(letterMap.has(listName[i])){  //si al map te la lletra 
+        letterMap.set(listName[i], letterMap.get(listName[i])+1 ); //afegeix amb map el contigut que te +1
+    }else{
+        letterMap.set(listName[i],1);//sino afegeix la clau i el valor 1
+    }
+}
 
+console.log(letterMap);
+
+/*fase3 sin map*/
+/*
+var newLetter= [];
+
+for (var i=0; i<listName.length; i++ ){
+    console.log(newLetter);
+    
+    if(newLetter.includes(listName[i]+ ": 1")){
+        console.log(listName[i]);
+        var num=1;
+        newLetter.push(listName[i] + ": "+ (num+1));
+        num++;
+    }else{
+        newLetter.push(listName[i] + ": 1");
+    }
+}
+console.log(newLetter);
+*/
 
 /*fase4*/
+listName.pop(); //li trec el numero 3 que havia afegit a fase2
 
 var surname=["t","e","r","m","e","n","s"];
-console.log(surname);
+
+/* 
 for(i=0; i<surname.length; i++){
-    listName.push(surname[i]);
-   
+    listName.push(surname[i]);  
 }
-console.log(nameList);
+console.log(listName);
+*/
+
+var fullName=[];
+fullName= listName.concat(surname);
+console.log(fullName);
+
+fullName.splice(listName.length,0, " ");
+console.log(fullName);
 
 
 
